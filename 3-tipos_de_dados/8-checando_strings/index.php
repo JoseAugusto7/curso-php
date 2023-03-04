ad62403da50e8517d0d34098326d2b996f0e6028
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-04 18:34:27 -0300

    Checando strings

---
+++ 3-tipos_de_dados/8-checando_strings/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $str = "José";
+    $num = 10;
+
+    if (is_string($str)) {
+        echo "$str é uma string! <br>";
+    }
+
+    if (is_string($num)) {
+        echo "$num é uma string! 2 <br>";
+    }
+
+    if (is_string("agf")) {
+        echo "É uma string! 3 <br>";
+    }
+
+?>
