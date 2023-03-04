5e7889af270ef442665950ca0795f29132285d1e
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-04 14:08:37 -0300

    Palavras Reservadas

---
+++ 2-conceitos_básicos/6-palavras_reservadas/index.php
@@ -0,0 +1,19 @@
+<?php
+
+     /* As funções abaixo dariam em um erro por usar palavras reservadas do próprio PHP.
+        Evitar usar palavras reservadas, se precisar muito usar, use com variações como
+        no último exemplo.
+     
+     function echo() {
+         return true;
+     }
+
+     function while() {
+         return true;
+     } */
+
+    function echoNew() {
+        return true;
+    }
+
+?>
