3426e51ca715739e40476b5b053c3718aadf768b
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-04 13:23:01 -0300

    Case Sensitivy

---
+++ 2-conceitos_básicos/1-case_sensitivy/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Não é case sensitivy
+    echo "Teste <br>";
+    eCho "Testando 2 <br>";
+    ECHO "Testando 3 <br>";
+
+    // É case sensitivy
+    $nome = "José Augusto";
+    $NOME = "Teste";
+
+    echo $nome;
+    echo "<br>";
+    echo $NOME;
+
+?>
