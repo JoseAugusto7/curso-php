b6546dbb78c291079ba6e60a249fc48462266b20
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-04 18:12:06 -0300

    Exercício 4

---
+++ 3-tipos_de_dados/6-ex4/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Imprima 3 floats; Utilize a função is_float() em um deles.
+
+    echo 5.7;
+    echo "<br>";
+    echo -7.7;
+    echo "<br>";
+    if (is_float(-8.3)) {
+        echo "É um float!";
+    }
+
+?>
