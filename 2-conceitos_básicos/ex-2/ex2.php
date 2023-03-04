942ad042dfb44d4e3eaef3f4e9aae0db911a5ea6
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-04 14:02:11 -0300

    Exercício 2

--- 2-conceitos_básicos/ex-2/ex2.php
+++ 2-conceitos_básicos/ex-2/ex2.php
@@ -1,9 +1,25 @@
 <?php
 
     /* Faca:
-        É feita com ferro damasco com multiplas camadas
+        É feita com Aço damasco com multiplas camadas
         O cabo é de madeira de Pinheiro
         Tem uma serra na parte de cima.
     */
+
+    // Aqui está o material que é feito a faca.
+    $faca = "Aço Damasco";
+
+    // Material do cabo.
+    $cabo = "Madeira de Pinheiro";
+
+    // Característica diferente da faca.
+    $caract = "Serra";
+
+    echo $faca;
+    echo "<br>";
+    echo $cabo;
+    echo "<br>";
+    echo $caract;
+    echo "<br>";
 
 ?>
