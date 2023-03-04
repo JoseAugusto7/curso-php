b3870ac252bf2eb0b985b728064cb2dfa566bccb
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-04 19:40:38 -0300

    Arrays

---
+++ 3-tipos_de_dados/12-arrays/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    $a = [1, 2, 3];
+
+    // echo $a; Está função não serve para imprimir um array, confira a debaixo que é a ideal.
+
+    print_r($a);
+
+    echo "<br>";
+
+    echo $a[0]; // Neste caso estou mandando imprimir o primeiro número do array que no casos é o "1".
+    
+    echo "<br>";
+    
+    $arr = ["José", 1500, 1.84, true];
+    
+    print_r($arr);
+
+    echo "<br>";
+
+    echo $arr[2];
+
+?>
