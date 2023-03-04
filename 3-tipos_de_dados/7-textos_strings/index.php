503dda7218aeafcf786880314d9136931fb05e49
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-04 18:28:04 -0300

    Strings

---
+++ 3-tipos_de_dados/7-textos_strings/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    echo "Texto com aspas duplas <br>";
+    echo 'Texto com aspas simples <br>';
+    echo "Ele disse: 'Olá!' <br>";
+    echo 'Ele disse: "Olá!" <br>';
+
+    $a = 15;
+
+    echo "Ele tem $a anos <br>";
+    echo 'Ele tem $a anos <br>';
+
+
+?>
