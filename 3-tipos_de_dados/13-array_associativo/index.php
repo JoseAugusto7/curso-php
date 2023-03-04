e7a7a54983fab29a49a178646502835179f142fe
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-04 20:02:31 -0300

    Arrays_associativos

---
+++ 3-tipos_de_dados/13-array_associativo/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $arr = ['nome' => 'Pérola', 'estilo' => 'Gato', 'cor' => 'branco', 'patas' => 4];
+
+    print_r($arr);
+    echo "<br>";
+
+    echo $arr['nome'];
+    echo "<br>";
+    echo $arr['estilo'];
+    echo "<br>";
+    echo $arr['cor'];
+    echo "<br>";
+    echo $arr['patas'];
+
+
+?>
