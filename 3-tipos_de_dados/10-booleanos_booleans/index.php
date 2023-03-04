644288851eeaa27775508510408a5cb85876cec5
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-04 19:10:32 -0300

    Booleans

---
+++ 3-tipos_de_dados/10-booleanos_booleans/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo true;
+    echo "<br>";
+    echo false;
+    echo"<br>";
+
+    if (true) {
+        echo "É verdadeiro! <br>";
+    }
+
+    if (5 > 2) { // true
+        echo "É verdadeiro! <br>";
+    }
+
+    if (5 < 2) { // false
+        echo "É verdadeiro! <br>";
+    }
+
+?>
