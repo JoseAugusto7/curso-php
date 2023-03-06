35b66f27468c5503a9cd295d90767c0b5a7de042
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 15:23:00 -0300

    Exercício 14

---
+++ 5-operadores_e_expressoes/11-ex14/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Crie três variáveis, saudacao, nome e sobrenome. Imprima as três de uma maneira que faça sentido.
+
+    $saudacao = "Olá";
+    $nome = "João";
+    $sobrenome = "Oliveira";
+
+    echo $saudacao . " " . $nome . " " . $sobrenome . "!";
+
+?>
