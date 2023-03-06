68bb01b7d42c9619dd10556759ab988a87518860
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 14:39:21 -0300

    Operadores Aritméticos

---
+++ 5-operadores_e_expressoes/5-operadores_aritmeticos/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $soma = 7 + 7;
+    echo $soma . "<br>";
+
+    $subt = 7 - 7;
+    echo $subt . "<br>";
+
+    $mult = 7 * 7;
+    echo $mult . "<br>";
+
+    $divi = 7 / 7;
+    echo $divi . "<br>";
+
+    $op = 7 * 7 / (7 + 7 - 7);
+    echo $op . "<br>";
+
+?>
