7be67e6391b324aa45644c0b2b9f8dddb870d82a
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 14:54:24 -0300

    Operador Módulo

---
+++ 5-operadores_e_expressoes/7-operador_modulo/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo 12 % 2;  // O sinal de modulo "%" serve para mostrar o resto da divisão de dois números.
+    echo " modulo <br>";
+    echo 12 / 2;
+    echo " divisão <br>";
+
+    echo 13 % 5 . "<br>";
+    echo 20 % 12 . "<br>";
+
+?>
