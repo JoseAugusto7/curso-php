e2a1c357464933e5248bf20726f1cbe4e7eb192b
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 14:46:21 -0300

    Exercício 12

---
+++ 5-operadores_e_expressoes/6-ex12/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Crie uma operação com cada um dos operadores básicos ( + - * /)
+    // Cada operação deve estar em uma variável diferente, depois imprima-os na tela.
+
+    $soma = 5 + 10;
+    $subt = 20 - 5;
+    $mult = 3 * 5;
+    $divi = 45 / 3;
+
+    echo $soma . "<br>";
+    echo $subt . "<br>";
+    echo $mult . "<br>";
+    echo $divi . "<br>";
+
+?>
