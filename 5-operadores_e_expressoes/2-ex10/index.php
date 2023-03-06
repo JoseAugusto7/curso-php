043581da4909f4ddc0947dbd826891135efbc81f
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 12:09:15 -0300

    Exercício 10

---
+++ 5-operadores_e_expressoes/2-ex10/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    // Crie uma operação que utilize subtração (-), divisão (/) e multiplicação (*).
+    // Armazene os valores em variáveis e depois imprima-os na tela.
+
+    echo 50 - 1 * 50 / 2; // Operação com todos os operadores pedidos.
+    echo "<br>";
+
+    $a = 50; // Todos os valores armazenados em variáveis.
+    $b = 1;
+    $c = 2;
+
+    $d = $a - $b * $a / $c; // Armazendando o resultado em uma variável e imprimindo na tela.
+    echo $d;
+    echo "<br>";
+
+?>
