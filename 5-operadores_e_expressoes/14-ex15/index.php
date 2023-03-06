b3895b8275c3f28ec62c8ee6ec158901f82d1e8e
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 18:23:41 -0300

    Exercício 15

---
+++ 5-operadores_e_expressoes/14-ex15/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    // Crie duas operações com igualdade, uma falsa e uma verdadeira.
+
+    $a = 7;
+    $b = 7;
+    $c = 14;
+
+    if ($b == $a) {
+        echo "Esta é a verdadeira. <br>";
+    }
+
+    if ($a == $c) {
+        echo "Esta é a falsa. <br>";
+    }
+
+?>
