4a4e18dce89d964f36fa522a796d78c3dcce1c3d
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 19:54:29 -0300

    Exercício 16

---
+++ 5-operadores_e_expressoes/18-ex16/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    // Faça duas variáveis, uma com o valor 5 e outra com o valor 3.
+    // Teste os operadores: ==, !=, ===, !==.
+
+    $a = 5;
+    $b = 3;
+
+    if ($a == $b) {
+        echo "A é igual a B <br>";
+    }
+
+    if ($a != $b) { // A única verdadeira é essa operação.
+        echo "A é diferente de B <br>";
+    }
+
+    if ($a === $b) {
+        "A é idêntico a B <br>";
+    }
+
+    if ($a !== $b) {
+        "A não é idêntico a B <br>";
+    }
+
+?>
