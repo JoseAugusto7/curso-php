dc7894b9e3fa71d184811f2171d417508d25cf23
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-05 21:33:23 -0300

    Exercício 8

---
+++ 4-variaveis/2-ex8/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    // Crie três variáveis com tipos de dados diferentes e imprima-os na tela.
+
+    $nome = "José";
+    $idade = 23;
+    $altura = 1.82;
+
+    echo "Seu nome é $nome.";
+    echo "<br>";
+    echo "Você tem $idade anos.";
+    echo "<br>";
+    echo "Você tem $altura m de altura";
+
+?>
