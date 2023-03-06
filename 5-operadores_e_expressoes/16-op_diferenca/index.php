d41b47620209d00a9603766876405b1d2bfa0123
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 18:35:33 -0300

    Operadores de diferença

---
+++ 5-operadores_e_expressoes/16-op_diferenca/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $a = 15;
+    $b = 20;
+    $c = 15;
+
+    if ($a != $b) {
+        echo "Os valores são diferentes 1 <br>";
+    }
+
+    if ($a != $c) {
+        echo "Os valores são diferentes 2 <br>";
+    }
+
+?>
