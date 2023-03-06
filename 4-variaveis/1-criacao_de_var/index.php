edb13a9c51f52ec3aff61ce94ba8d67344bb41bb
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-05 21:28:15 -0300

    Formas de declarar variáveis

---
+++ 4-variaveis/1-criacao_de_var/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $teste = "Algum texto";
+    $num = 15;
+    $velocidadeMaxima = 100;
+    $velocidade_minima = 50;
+
+    echo $teste;
+    echo "<br>";
+    echo $num;
+    echo "<br>";
+    echo $velocidadeMaxima;
+    echo "<br>";
+    echo $velocidade_minima;
+
+?>
