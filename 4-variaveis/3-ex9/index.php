a198d4caa8e5b3ba87381935707edf0a0235747e
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-05 21:37:31 -0300

    Exercício 3

---
+++ 4-variaveis/3-ex9/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Crie duas variáveis com números, depois crie outra variável com a soma desses números.
+
+    $a = 5; // int
+    $b = 7.5; // float
+    $c = $a + $b;
+
+    echo $c;
+
+?>
