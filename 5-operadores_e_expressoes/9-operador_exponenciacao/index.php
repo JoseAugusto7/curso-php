fc3cc6c6263c6532bcb5b0fcfca306f4c778c313
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 15:07:24 -0300

    Operador de Exponenciação

---
+++ 5-operadores_e_expressoes/9-operador_exponenciacao/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo 5 ** 2 . "<br>"; // Neste caso temos a mesma coisa que 5² que seria 5*5.
+    echo 2 ** 10 ."<br>";
+
+    $a = 5;
+    $b = 8;
+
+    echo $b ** $a. "<br>";
+
+    $c = $a ** $b;
+
+    echo $c . "<br>";
+
+
+
+?>
