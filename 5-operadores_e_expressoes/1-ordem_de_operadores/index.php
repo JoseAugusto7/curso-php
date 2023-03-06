285f28a0a4190ce2a8d37c886eee9b6851bbd1ab
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 11:48:12 -0300

    Ordem de Operadores

---
+++ 5-operadores_e_expressoes/1-ordem_de_operadores/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    echo 3 + 5 * 7;
+    echo "<br>";
+    echo (3 + 5) * 7;
+    echo "<br>";
+
+    $a = 3;
+    $b = 5;
+    $c = 7;
+
+    echo $a + $b * $c;
+    echo "<br>";
+    echo ($a + $b) * $c;
+    echo "<br>";
+
+    $d = ($a + $b) * $c;
+    $e = $c * $b + $a;
+
+    echo $d;
+    echo "<br>";
+    echo $e;
+
+
+?>
