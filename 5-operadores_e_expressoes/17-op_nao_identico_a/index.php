16c58f9d488ffd8dbaa6355e39c113d5cf964d14
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 18:41:10 -0300

    Operador não identico a

---
+++ 5-operadores_e_expressoes/17-op_nao_identico_a/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $a = "15";
+    $b = 15;
+    $c = "15";
+
+    if ($a !== $c) {
+        echo "O valor não é idêntico 1 <br>";
+    }
+
+    if ($a !== $b) {
+        echo "O valor não é idêntico 2 <br>";
+    }
+
+?>
