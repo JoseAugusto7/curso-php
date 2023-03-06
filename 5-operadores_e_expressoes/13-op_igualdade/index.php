f8ffcd0903d74d2091c16913414ed56aced08eac
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 18:20:35 -0300

    Operador de igualdade

---
+++ 5-operadores_e_expressoes/13-op_igualdade/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $a = 15;
+    $b = 15;
+    $c = 100;
+
+    if ($a == $b) {
+        echo "O valor é igual 1 . <br>";
+    }
+
+    if ($a == $c) {
+        echo "O valor é igual 2 . <br>";
+    }
+
+    $nome = "José";
+    $nomeDoSistema = "José";
+
+    if ($nome == $nomeDoSistema) {
+        echo "O nome coincide. <br>";
+    }
+
+?>
