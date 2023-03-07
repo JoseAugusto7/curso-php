280cb4197ac75c40f025c6ae345c0390b03e4d02
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 23:05:51 -0300

    Maior ou igual

---
+++ 5-operadores_e_expressoes/19-maior_maior_ou_igual/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    $a = 5;
+    $b = 6;
+    $c = 7;
+    $d = 7;
+    $e = 8;
+
+    if ($a > $b) {
+        echo "A maior que B <br>";
+    }
+    
+    if ($b > $a) {
+        echo "B maior que A <br>";
+    }
+
+    if ($d >= $c) {
+        echo "D maior ou igual a C <br>";
+    }
+
+    if ($d >= $e) {
+        echo "D maior ou igual a E <br>";
+    }
+
+?>
