c703fe532822eef7ede8035e1c1c9e349d676b47
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-07 18:57:53 -0300

    Exercício 22

---
+++ 6-estruturas_de_controle/3-ex22/index.php
@@ -0,0 +1,27 @@
+<?php
+
+    /*
+        Crie variáveis que recebem idade;
+        Cheque se as idades são maiores ou iguais a 18;
+        Se sim, imprima uma mensagem que a pessoa é maior de idade;
+    */
+
+    $menor = 15;
+    $igual = 18;
+    $maior = 35;
+    $maioridade = 18;
+
+    if ($menor >= $maioridade) {
+        echo "Você é maior de idade 1 <br>";
+    }
+
+    if ($igual >= $maioridade) {
+        echo "Você é maior de idade 2 <br>";
+    }
+
+    if ($maior >= $maioridade) {
+        echo "Você é maior de idade 3 <br>";
+    }
+
+
+?>
