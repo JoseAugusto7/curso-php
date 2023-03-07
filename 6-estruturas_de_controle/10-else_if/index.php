81c6147a04dcc4102d156ca39fcfdeb6c08bb566
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-07 20:19:21 -0300

    Else if

---
+++ 6-estruturas_de_controle/10-else_if/index.php
@@ -0,0 +1,31 @@
+<?php
+
+    if (5 > 2) {
+        echo "Entrou no if";
+    } else if (5 > 4) {
+        echo "Entrou no else if";
+    } else {
+        echo "Entrou no else";
+    }
+
+    echo "<br>";
+
+    if (5 < 2) {
+        echo "Entrou no if";
+    } else if (5 > 4) {
+        echo "Entrou no else if";
+    } else {
+        echo "Entrou no else";
+    }
+
+    echo "<br>";
+
+    if (5 > 2) {
+        echo "Entrou no if";
+    } else if (5 < 4) {
+        echo "Entrou no else if";
+    } else {
+        echo "Entrou no else";
+    }
+
+?>
