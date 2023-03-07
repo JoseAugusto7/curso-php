58cd0d3e706217839d9af90f5a968b13e9142812
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-06 23:11:01 -0300

    Menor ou igual

---
+++ 5-operadores_e_expressoes/20-menor_menor_ou_igual/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $a = 15;
+    $b = 7;
+    $c = 70;
+
+    if ($a <= $b) {
+        echo "A é igual ou menor que B <br>";
+    }
+
+    if ($b < $c) {
+        echo "B é menor que C <br>";
+    }
+
+    if ($a <= $c) {
+        echo "A é menor ou igual a C <br>";
+    }
+
+?>
