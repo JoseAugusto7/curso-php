de69d56891c8664eb50dcbbf267368576cda1f7a
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-07 01:46:02 -0300

    Operadores ternarios

---
+++ 5-operadores_e_expressoes/29-op_ternario/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo 20 > 10 ? "Deu true" : "Deu false";
+
+    echo "<br>";
+
+    echo 20 < 10 ? "Deu true" : "Deu false";
+
+    echo "<br>";
+
+    $a = 20;
+    $b = 70;
+
+    echo $a < $b ? "Deu true" : "Deu false";
+
+    echo "<br>";
+
+    echo $a > $b ? "Deu true" : "Deu false";
+
+?>
