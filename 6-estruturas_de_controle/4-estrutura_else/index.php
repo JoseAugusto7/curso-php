87e2fab8370ede01e2adbc6caaeaf381c933008f
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-07 19:06:28 -0300

    Estrutura else

---
+++ 6-estruturas_de_controle/4-estrutura_else/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    if (5 > 2) {
+        echo "É verdadeiro <br>";
+    } else {
+        echo "É falso <br>";
+    }
+
+    if (5 === "João") {
+        echo "É verdadeiro <br>";
+    } else {
+        echo "É falso <br>";
+    }
+
+?>
