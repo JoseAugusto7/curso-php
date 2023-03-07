c7f980bed8a532f013b3b5faef7d05e667edf62b
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-07 01:11:00 -0300

    Operador NOT

---
+++ 5-operadores_e_expressoes/25-op_not/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    if (5 > 2) {
+        echo "1 É verdadeiro <br>";
+    }
+
+    if (!(5 > 2)) { // De verdadeiro foi para falso com o operador NOT (!)
+        echo "2 É verdadeiro <br>";
+    }
+
+    if (!(100 < 5)) { // De falso foi para verdadeiro da mesma forma.
+        echo "3 É verdadeiro";
+    }
+
+?>
