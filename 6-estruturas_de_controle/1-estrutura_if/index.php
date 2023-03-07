c852b7fda3587fb35e01f95efdde49575f7fbb52
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-07 18:41:38 -0300

    Estrutura if

---
+++ 6-estruturas_de_controle/1-estrutura_if/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Condição verdadeira
+
+    if (5 > 2) {
+        echo "Deu verdadeiro então vai imprimir na tela <br>";
+    }
+
+    if (5 < 2) {
+        echo "Vai ser falso então não irá imprimir na tela.";
+    }
+
+?>
