1c7d6e82872047de86207a0fa69ef47e1f42dead
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-07 20:42:26 -0300

    Estrutura switch

---
+++ 6-estruturas_de_controle/12-estrutura_switch/index.php
@@ -0,0 +1,46 @@
+<?php
+
+    $x = 0;
+    $y = 1;
+    $w = 5;
+
+    switch ($x) {
+        
+        case 0:
+            echo "X é igual a 0 <br>";
+            break;
+        case 1: 
+            echo "X é igual a 1 <br>";
+            break;
+        default: 
+            echo "X não é igual a nenhum dos valores <br>";
+
+    }
+
+    switch ($y) {
+        
+        case 0:
+            echo "X é igual a 0 <br>";
+            break;
+        case 1: 
+            echo "X é igual a 1 <br>";
+            break;
+        default: 
+            echo "X não é igual a nenhum dos valores <br>";
+
+    }
+
+    switch ($w) {
+        
+        case 0:
+            echo "X é igual a 0 <br>";
+            break;
+        case 1: 
+            echo "X é igual a 1 <br>";
+            break;
+        default: 
+            echo "X não é igual a nenhum dos valores <br>";
+
+    }
+
+?>
