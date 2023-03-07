57ae8746f1ad6b2b44761a33d8ce5c5993f6670a
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-07 01:51:43 -0300

    Exercício 20

---
+++ 5-operadores_e_expressoes/30-ex20/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Atribua dois números a variáveis distintas;
+    // Faça uma comparação de maior ou igual com operador ternário;
+    // Imprima resultados para ambas possibilidades.
+
+    $a = 70;
+    $b = 7;
+
+    echo $a >= $b ? "Deu true" : "Deu false";
+    echo "<br>";
+    echo $b >= $a ? "Deu true" : "Deu false";
+
+?>
