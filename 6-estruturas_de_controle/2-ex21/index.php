242240b789668c44976e5003349f6469923e87d0
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-07 18:47:26 -0300

    Exercício 21

---
+++ 6-estruturas_de_controle/2-ex21/index.php
@@ -0,0 +1,28 @@
+<?php
+
+    /* Faça as seguintes verificações em estruturas if:
+       5 é maior que 2;
+       Matheus é diferente de pedro;
+       12 é menor ou igual a 11;
+       Os valores devem ser inseridos em variáveis. */
+       
+       $a = 5;
+       $b = 2;
+       $c = "Matheus";
+       $d = "Pedro";
+       $e = 12;
+       $f = 11;
+
+       if ($a > $b) {
+        echo "A é maior que B. <br>";
+       }
+
+       if ($c != $d) {
+        echo "Matheus é diferente de Pedro. <br>";
+       }
+
+       if ($e <= $f) {
+        echo "12 é menor ou igual a 11. <br>";
+       }
+
+?>
