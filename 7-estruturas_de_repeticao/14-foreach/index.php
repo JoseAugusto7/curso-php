00937c855fa56e4cc4c058279ab1bf65ce74d62a
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-08 18:55:06 -0300

    foreach

---
+++ 7-estruturas_de_repeticao/14-foreach/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nomes = ["José", "Maria", "João", "Pedro", "Sophia"];
+
+    foreach ($nomes as $nome) {
+
+        echo "O nome do índice atual é $nome! <br>";
+
+        if ($nome == "Maria") {
+            echo "Opa! <br>";
+        }
+
+    }
+
+?>
