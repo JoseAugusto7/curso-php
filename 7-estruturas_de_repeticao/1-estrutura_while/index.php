73be55adc7a4414417d56eb3f9400dbd614bbde7
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-08 16:15:13 -0300

    Estrutura while

---
+++ 7-estruturas_de_repeticao/1-estrutura_while/index.php
@@ -0,0 +1,36 @@
+<?php
+
+    $x = 0;
+
+    while ($x < 10) {
+        echo $x . "<br>";
+
+        $x ++;
+    }
+
+    echo "Continuando código <br>";
+
+    $y = 10;
+
+    while ($y > 0) {
+        echo $y . "<br>";
+
+        $y --;
+    }
+
+    echo "Continuando código";
+
+    $z = 20;
+
+    while ($z > 0) {
+
+        if ($z % 2 != 0) {
+            echo $z . "<br>";
+        }
+
+        $z --;
+
+    }
+
+
+?>
