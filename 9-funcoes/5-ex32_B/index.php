c3e0f7b130924bd7538b9ccfa6a0e4bf7c9f4887
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-09 16:39:59 -0300

    Exercício 32 B

---
+++ 9-funcoes/5-ex32_B/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    /*
+        Crie uma função;
+        Defina uma variável nome e uma sobrenome;
+        Imprima os valores concatenados.
+    */
+
+    function nomeCompleto() {
+
+        $nome = "José";
+        $sobrenome = "Morado";
+
+        $nomeCompleto = $nome . " " . $sobrenome . "<br>";
+
+        echo $nomeCompleto;
+
+    }
+
+    nomeCompleto();
+
+?>
