aa60cf9959be8cddeab9cba4dd0fb34408aa915a
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-09 16:11:55 -0300

    Chamando funções

---
+++ 9-funcoes/2-chamando_funcoes/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    // Definindo uma função
+
+    function soma() {
+
+        $a = 7;
+        $b = 63;
+
+        echo $a + $b . "<br>";
+
+    }
+
+    // Chamando / Invocando, uma função que definimos.
+
+    soma();
+    soma();
+
+    // Chamando uma função do próprio PHP.
+
+    echo strtoupper("testando letras maiúsculas.");
+
+?>
