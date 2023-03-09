30f42314521f33ec78f51171d0a98883c3ded9db
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-09 17:41:02 -0300

    Exercício 35

---
+++ 9-funcoes/10-ex35/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    /*
+        Crie uma função que recebe um número;
+        Retorne o valor desse número ao quadrado;
+    */
+
+    function potencia($num) {
+
+        return $num ** 2;
+
+    }
+
+    echo potencia(5);
+
+?>
