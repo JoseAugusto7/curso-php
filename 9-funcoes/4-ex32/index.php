8125aef60de7751a259f1159d5030a1c45c4ac43
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-09 16:35:23 -0300

    Exercício 32

---
+++ 9-funcoes/4-ex32/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    /*
+        Crie uma função;
+        Defina três variáveis numéricas dentro dela;
+        Exiba a multiplicação destes itens com um echo;
+    */
+
+    function mult() {
+
+        $a = 7;
+        $b = 2;
+        $c = 10;
+
+        echo $a * $b * $c;
+
+    }
+
+    mult();
+
+?>
