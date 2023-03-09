26b5aa674c119929735164cf57b095b44e2f2372
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-09 17:25:45 -0300

    Retorno de função

---
+++ 9-funcoes/9-retorno_de_funcao/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    function soma($n1, $n2) {
+
+        return $n1 + $n2;
+
+    }
+
+    echo soma(5, 7) . "<br>";
+
+    $x = soma(7, 7);
+
+    $y = soma($x, 7);
+
+    echo $x . "<br>";
+    echo $y . "<br>";
+
+
+?>
