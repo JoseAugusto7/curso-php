96f508dd871d8728329a8a5448d9f37ee568ac9a
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-09 15:16:01 -0300

    Short tag

---
+++ 8-inclusao_de_codigo/4-short_tag/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo "Testando tag normal <br>";
+
+?>
+
+<? // Para habilitar esta short tag, eu tive que ir em config no servidor, entrar na pasta no php.ini, procurar por short_open e habilitar a opção. Não é recomendado mas é uma experiência a mais. Use a tag normal.
+
+    echo "Testando short tag";
+
+?>
