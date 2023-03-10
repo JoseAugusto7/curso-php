772e0dd0015a9384d39701cf9efc0889812578df
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 19:54:45 -0300

    Retornando resto da string

---
+++ 10-strings/24-retornando_resto_string/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $str = "Testando o resto da string para ver se da certo";
+
+    $resto = strstr($str, "string"); // Neste caso a função vai pegar da palavra string para frent.
+
+    echo $resto;
+
+?>
