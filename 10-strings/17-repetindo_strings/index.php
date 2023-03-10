94ab47ee6eb2abbceea5d1a5f2676935170c06cc
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 19:03:16 -0300

    Repetindo strings

---
+++ 10-strings/17-repetindo_strings/index.php
@@ -0,0 +1,7 @@
+<?php
+
+    $str = "Repetindo strings <br>";
+
+    echo str_repeat($str, 5); // A primeira condição é a string que você deseja repetir, a segunda é o número de vezes (precisa ser um número inteiro)
+
+?>
