c709020aef2e2c050786d0d5b7d2163a6dc9a2ef
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 18:06:46 -0300

    Case de palavras

---
+++ 10-strings/11-alterando_case_de_palavras/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $frase = "testando case de palavras <br>";
+
+
+    // Primeira palavra da string em maiúsculo, se já estiver nada vai mudar.
+    echo ucfirst($frase);
+
+
+    // Primeira palavra de todas as letras em maiúsculo.
+    echo ucwords($frase);
+
+?>
