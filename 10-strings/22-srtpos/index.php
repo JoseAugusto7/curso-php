f921e5dbd6afc053a7f958efe6a6a7aad24c0362
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 19:38:25 -0300

    Procurando string dentro da string

---
+++ 10-strings/22-srtpos/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $str = "Estamos testando a função strpos, com ela podemos encontrar uma string escolhida dentro de outra string";
+
+    $teste = strpos($str, "strpos"); // No primeiro argumento coloque o nome da string onde vai procurar, no segundo coloque qual string procurar.
+
+    echo $teste; // Pode ver que ele retorna a posição onde começa a string que eu pedi.
+
+?>
