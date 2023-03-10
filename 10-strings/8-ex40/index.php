13a5ae13f2403ad145aebcc88d4e40b34389c7fa
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 17:19:45 -0300

    Exercício 40

---
+++ 10-strings/8-ex40/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    /*
+        Percorra a string: o rato roeu a roupa do rei de Roma, a partir de um loop;
+        Imprima o número de letras "a" desta string.
+    */
+
+    $roer = "O rato roeu a roupa do rei de Roma";
+
+    for ($a = 0; $a <= strlen($roer); $a ++) {
+
+        if ($a = strlen($roer)) {
+            echo $a;
+        }
+
+    }
+
+?>
