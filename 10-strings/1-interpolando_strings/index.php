f54d8da316ab9aecbd67e35a70324d9cd3730ac1
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 15:49:02 -0300

    Interpolando strings

---
+++ 10-strings/1-interpolando_strings/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nome = "José";
+    $profissão = "Programador";
+    $idade = 23;
+
+    echo "Me chamo $nome, atuo como $profissão e tenho $idade anos.";
+
+    echo "Me chamo {$nome}, atuo como {$profissão} e tenho {$idade} anos.";
+
+    // Não tem diferença entre as duas interpolações, porém é recomendado que se utilize apenas uma das sintaxes em um projeto ou algo do tipo.
+
+?>
