277940c3b90ec21d1d36ad259ee490bc0c5346b8
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 16:02:42 -0300

    Valores de escape

---
+++ 10-strings/2-valores_de_escape/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    header("Content-type: text/plain"); // Aqui eu digo pro php que o conteúdo abaixo é texto puro. Preciso fazer isso para as escapes funcionarem.
+
+    // Pular linha
+
+    echo "Isso aqui fica na primeira linha \n e isso aqui na segunda \n";
+
+    // Tab
+
+    echo "Testando o tab \t aqui \n";
+
+    // Barra invertida
+
+    echo "Barra invertida \\ \n";
+
+    // Sinal de dólar
+
+    echo "Imprimindo o dólar \$";
+
+?>
