f36ec75db09cbdfdce26c55a69eafe121a561c72
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 18:00:23 -0300

    Alterando case

---
+++ 10-strings/10-alterando_case/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $str1 = "Esta string está em caixa alta <br>";
+
+    echo strtoupper($str1);
+
+    $str2 = "Esta string está em caixa baixa <br>";
+
+    echo strtolower($str2);
+
+?>
