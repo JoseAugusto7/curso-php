d066258adcf4b72a369db056498d971d328ccbd6
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 18:42:25 -0300

    Exercício 42

---
+++ 10-strings/15-ex42/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    /*
+        Na frase: "Cade o meu queijo? Ele estava aqui em cima";
+        Resgate apenas a palavra "queijo".
+    */
+
+    $frase = "Cade o meu queijo? Ele estava aqui em cima";
+
+    echo substr($frase, 11, 6);
+
+?>
