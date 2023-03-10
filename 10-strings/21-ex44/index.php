363dabc7753c8aca84643e52686dc30630af0017
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-10 19:28:17 -0300

    Exercício 44

---
+++ 10-strings/21-ex44/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    /*
+     Converta a seguinte array para uma string;
+     ["O", "PHP", "é", "muito", "legal"].
+    */
+
+    $arr = ["O", "PHP", "é", "muito", "legal"];
+
+    $frase = implode(", ", $arr);
+
+    echo $frase;
+
+?>
