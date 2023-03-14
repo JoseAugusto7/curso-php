886b476896ca16c1253f2d285950c0eae8e1d3ae
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-14 17:16:43 -0300

    Contando o número de itens do array

---
+++ 11-arrays/5-numero_elementos_do_array/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Podemos contar o número de elementos de um array com a função "count()", use o array como argumento da função e ela retornará um valor inteiro que é o número de itens do array.
+
+    $arr = [1, 2, 3];
+    $arr2 = range(1, 100);
+
+    echo count($arr) . "<br>";
+    echo count($arr2) . "<br>";
+
+?>
