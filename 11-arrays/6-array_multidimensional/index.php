99411b16704cd6c8d8775b46b4543703c316c7d8
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-14 17:35:36 -0300

    Array multidimensional

---
+++ 11-arrays/6-array_multidimensional/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $arr = [
+        [1, 2, 3],
+        [5, 10, 15]
+    ];
+
+    echo $arr[0][2]; // Acessando o terceiro item dentro do primeiro array dentro do array principal.
+    echo "<br>";
+    echo $arr[1][1]; // Acessando o segundo item dentro do segundo array dentro do array principal.
+
+?>
