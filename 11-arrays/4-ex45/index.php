d353cd2d6e419b6ca926da3c8c53997580fbe6d9
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-14 17:10:47 -0300

    Exercício 45

--- 11-arrays/4-ex45/index.php
+++ 11-arrays/4-ex45/index.php
@@ -10,10 +10,10 @@
 
     for ($s = 0; $s < count($arr); $s ++) {
 
-        if ($arr[$s] <= 30) {
-            echo $arr[$s] . "<br>";
+        if ($arr[$s] + 6 <= 30) {
+            echo $arr[$s] + 6 . "<br>";
         } else {
-            echo $arr[$s] . " O número é muito alto <br>";
+            echo $arr[$s] + 6 . " O número é muito alto <br>";
         }
 
     }
