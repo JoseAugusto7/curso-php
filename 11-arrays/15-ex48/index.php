7a128adcb42d30e957b545bc03e22383adf19dd4
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-14 20:45:35 -0300

    Exercício 48

---
+++ 11-arrays/15-ex48/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    /*
+        Crie um array com os valores: batata, maçã, pêra, feijão e arroz;
+        Remova pêra e feijão.
+    */
+
+    $compras = ["batata", "maçã", "pêra", "feijão", "arroz"];
+
+    $comprados = array_splice($compras, 2, 2);
+
+    print_r($compras);
+
+    echo "<br>";
+
+    print_r($comprados);
+
+?>
