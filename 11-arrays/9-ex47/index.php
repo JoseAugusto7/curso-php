7900c46e0ce567b8708afe866e424bd548f25173
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-14 18:43:32 -0300

    Exercício 47

---
+++ 11-arrays/9-ex47/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    /*
+        Crie um array com os seguintes valores: jaguar, 3.0, azul, 18, Teto solar, automático;
+        Chame este array de carro;
+        Crie variáveis com base neste array.
+    */
+
+    $carro = ["jaguar", "3.0", "azul", 18, "Teto solar", "automático"];
+
+    list($marca, $pot, $cor, $t, $teto, $caimbro) = $carro;
+
+    echo "$marca <br>";
+    echo "$pot <br>";
+    echo "$cor <br>";
+    echo "$t <br>";
+    echo "$teto <br>";
+    echo "$caimbro <br>";
+
+?>
