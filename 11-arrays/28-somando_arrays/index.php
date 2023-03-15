67dc871a32343cbfd757c77adad4941cd8d0087c
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-15 14:49:24 -0300

    Somando itens do array

---
+++ 11-arrays/28-somando_arrays/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $arr = [8.5, 7, 90, 526, 72.7, 48.9];
+
+    $soma = array_sum($arr);
+
+    print_r($arr);
+    echo "<br>";
+    print_r($soma);
+
+    // Se for colocado algum dado que não seja numérico ele irá ignorar.
+
+?>
