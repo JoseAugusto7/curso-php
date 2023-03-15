c5bcadae1676301e68033afa7fe6046c5d406d4d
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-15 14:36:52 -0300

    Revertendo arrays

---
+++ 11-arrays/26-array_reverse/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $arr = [1, 7, 50, 3, 8, 75];
+    $arrRev = array_reverse($arr);
+
+    print_r($arr);
+    echo "<br>";
+    print_r($arrRev);
+
+    // Com o array_reverse conseguimos revertar qualquer tipo de array, funciona com todos os tipos de dados.
+
+?>
