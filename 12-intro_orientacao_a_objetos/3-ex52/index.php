4cf9e9378390fce8c26b59c73b41fa8909e71f13
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-15 18:04:58 -0300

    Exercício 52

---
+++ 12-intro_orientacao_a_objetos/3-ex52/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    /*
+        Crie uma classe Car;
+        Instâncie três objetos com essa classe.
+    */
+
+    class Car {
+
+
+
+    }
+
+    $corolla = new Car;
+    $mobi = new Car;
+    $civic = new Car;
+
+?>
