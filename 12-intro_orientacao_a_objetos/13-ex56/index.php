f41aae3242f084a78def3943c14dd04fb3f5867f
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-16 22:25:32 -0300

    Exercício 56

---
+++ 12-intro_orientacao_a_objetos/13-ex56/index.php
@@ -0,0 +1,44 @@
+<?php
+
+    /*
+        Crie uma classe Humano com algumas propriedades e o método falar();
+        Crie outra classe Professor que herda de humano, crie também as propriedades e métodos particulares dessa classe;
+        Exiba os valores das propriedades da classe pai e também utilize os métodos.
+    */
+
+    class Humano {
+
+        public $olhos = 2;
+        public $orelhas = 2;
+        public $bracos = 2;
+
+        function falar() {
+
+            echo "Olá, eu sou um ser humano.";
+
+        }
+
+    }
+
+    class Professor extends Humano {
+
+        public $tarefa = "Ensinar";
+        public $caract = "Paciência";
+
+        function darAula() {
+
+            echo "Ensinando outros humanos.";
+
+        }
+
+    }
+
+    $sophia = new Professor;
+
+    echo $sophia->olhos . "<br>";
+    echo $sophia->orelhas . "<br>";
+    echo $sophia->bracos . "<br>";
+
+    $sophia->falar();
+
+?>
