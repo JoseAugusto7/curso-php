ddd41d29e124b326e0ca7edf624baf7da39f02b7
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-17 23:12:31 -0300

    Usando a função data

---
+++ 13-datas/1-funcao_date/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $d = date("d/m/y"); // day / month / year
+    $d2 = date("D/M/Y"); // Perceba que há diferenças entre a letra maiúscula e a minúscula.
+    $d3 = date("d/M - Y"); // Também podemos colocar outras coisas alem de uma "/".
+
+    echo $d . "<br>";
+    echo $d2 . "<br>";
+    echo $d3 . "<br>";
+
+    // Para saber mais sobre, pesquise no google "date php".
+
+?>
