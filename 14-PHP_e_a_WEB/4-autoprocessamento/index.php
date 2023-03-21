2bbc4938767de1904ddabc4ea75a2d7d7b73465c
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-21 20:34:30 -0300

    Método POST

---
+++ 14-PHP_e_a_WEB/4-autoprocessamento/index.php
@@ -0,0 +1,48 @@
+<?php
+
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    if (isset($_POST["nome"])) {
+
+        $nome = $_POST["nome"];
+        $idade = $_POST["idade"];
+
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if ($method == "GET"):
+    ?>
+    <form action="index.php" method="POST">
+        <div>
+            <input type="text" name="nome" placeholder="Seu nome">
+        </div>
+        <div>
+            <input type="text" name="idade" placeholder="Sua idade">
+        </div>
+        <div>
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
+    <?php
+        else:
+    ?>
+    <h1>O seu nome é <?= $nome ?> e você tem <?= $idade ?> anos.</h1>
+    <?php
+        endif;
+    ?>
+
+
+</body>
+</html>
