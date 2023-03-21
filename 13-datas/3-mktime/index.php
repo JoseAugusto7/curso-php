05e9dc911e25873a12c25adf67f4a1a32dd5de7e
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-21 00:17:48 -0300

    Função mktime

---
+++ 13-datas/3-mktime/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $dataFutura = mktime(12, 15, 50, 03, 25, 2050);
+
+    echo $dataFutura . "<br>";
+
+    echo date("d/m/y", $dataFutura);
+
+    // Com a função mktime podemos criar uma data.
+
+?>
