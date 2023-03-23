f1dba6dc7a6dc4deaf30ae9818b433333ee63cf7
JoseAugusto7 <tails.augusto@hotmail.com>
2023-03-23 11:45:18 -0300

    Utilizando cookies

---
+++ 14-PHP_e_a_WEB/9-cookies/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    setcookie("nome", "jose", time() + 3600); // 3600 segundos.
+
+    if (isset($_COOKIE["nome"])) {
+        $nome = $_COOKIE["nome"];
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies</title>
+</head>
+<body>
+    <h1>Olá Mundo</h1>
+    <?php if($nome != ""): ?>
+        <p>Seja bem vindo <?= $nome ?> </p>
+    <?php endif; ?>
+</body>
+</html>
